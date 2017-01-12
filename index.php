4de4b4708c487ba9349ea1cb36cfe15fd8e5df8c
SmaRTeY <zilverballs@home.nl>
2017-01-12 19:57:13 +0100

    Update index.php
    RSS pubDate fix.
    Changed pubDate from date_modified to date_added

--- index.php
+++ index.php
@@ -300,7 +300,7 @@ if (iaView::REQUEST_XML == $iaView->getRequestType())
 		$output['item'][] = array(
 			'title' => $entry['title'],
 			'link' => $baseUrl . $entry['id'] . '-' . $entry['alias'],
-			'pubDate' => date('D, d M Y H:i:s T', strtotime($entry['date_modified'])),
+			'pubDate' => date('D, d M Y H:i:s T', strtotime($entry['date_added'])),
 			'description' => iaSanitize::tags($entry['body'])
 		);
 	}
